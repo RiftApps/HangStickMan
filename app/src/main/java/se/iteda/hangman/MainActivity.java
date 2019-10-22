faef002f912fa3af4e0e3533f223ca9ae1318c55
Timmy <timmy@iteda.se>
2019-10-22 18:03:37 +0200

    Rebase commit

--- app/src/main/java/se/iteda/hangman/MainActivity.java
+++ app/src/main/java/se/iteda/hangman/MainActivity.java
@@ -1,15 +1,36 @@
 package se.iteda.hangman;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, GameFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
 }
--- app/src/main/java/se/iteda/hangman/MenuFragment.java
+++ app/src/main/java/se/iteda/hangman/MenuFragment.java
@@ -1,14 +1,50 @@
 package se.iteda.hangman;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
-/**
-
- */
 public class MenuFragment extends Fragment {
 
+
     public MenuFragment() {
-        // Required empty public constructor
+
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_menu, container, false);
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Button btnNewGame = view.findViewById(R.id.btnMenuNewGameID);
+        Button btnAbout = view.findViewById(R.id.btnMenuAboutID);
+        btnNewGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController navController = Navigation.findNavController(getActivity(), R.id.menuFragment);
+                navController.navigate(R.id.gameFragment);
+            }
+        });
+    }
 }
+
+
